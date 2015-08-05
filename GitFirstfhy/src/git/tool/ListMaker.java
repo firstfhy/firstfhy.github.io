d290d7c8aae1bf8453ee37509a9560fd6f7c906c
hyfu <71806228>
2015-08-05 19:07:28 +0800

    list

--- GitFirstfhy/src/git/tool/ListMaker.java
+++ GitFirstfhy/src/git/tool/ListMaker.java
@@ -1,14 +1,25 @@
 package git.tool;
 
+import java.io.File;
+
 public class ListMaker extends GeneratedHTMLTemplate {
 	
-	public void make() {
-		
+	public void make() throws Exception {
+		File file = new File(BlogMaker.class.getResource("../../").toURI());
+		make(file);
+	}
+	
+	public void make(File file) throws Exception {
+		if (file.isDirectory()) {
+			
+		} else {
+			
+		}
 	}
 
 	@Override
 	public void make(String path, String name, String content, boolean existsDelete) throws Exception {
-		super.make("../../" + path, name, content, true);
+		super.make(path, name, content, true);
 	}
 
 }
